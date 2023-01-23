310e8033cbb6a1af4502b1e698e088a788e4e58b
Alok Raj <alokr417@gmail.com>
2023-01-23 20:12:25 +0530

    add employee management system code

---
+++ employee-management-system/src/Page/Dashboard/index.js
@@ -0,0 +1,86 @@
+import React, { useState } from 'react'
+import Swal from 'sweetalert2';
+
+import Header from './Header';
+import List from './List';
+import Add from './Add';
+import Edit from './Edit';
+
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteEmployee } from '../../redux';
+
+function Dashboard() {
+    const dispatch = useDispatch();
+    const { employees } = useSelector(state => state.employees);
+
+    console.log('TEST', employees)
+
+    const [selectedEmployee, setSelectedEmployee] = useState(null);
+    const [isAdding, setIsAdding] = useState(false);
+    const [isEditing, setIsEditing] = useState(false);
+
+    const handleEdit = (id) => {
+        const [employee] = employees.filter(employee => employee.id === id);
+
+        setSelectedEmployee(employee);
+        setIsEditing(true);
+    }
+
+    const handleDelete = (employee) => {
+        Swal.fire({
+            icon: 'warning',
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            showCancelButton: true,
+            confirmButtonText: 'Yes, delete it!',
+            cancelButtonText: 'No, cancel!',
+        }).then(result => {
+            if (result.value) {
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Deleted!',
+                    text: `${employee.firstName} ${employee.lastName}'s data has been deleted.`,
+                    showConfirmButton: false,
+                    timer: 1500,
+                });
+                dispatch(deleteEmployee(employee.id));
+            }
+        });
+    }
+
+
+    return (
+        <div className='container'>
+            {/* List */}
+            {!isAdding && !isEditing && (
+                <>
+                    <Header
+                        setIsAdding={setIsAdding}
+                    />
+                    <List
+                        employees={employees}
+                        handleEdit={handleEdit}
+                        handleDelete={handleDelete}
+                    />
+                </>
+            )}
+            {/* Add */}
+            {isAdding && (
+                <Add
+                    employees={employees}
+                    setIsAdding={setIsAdding}
+                />
+            )}
+            {/* Edit */}
+            {isEditing && (
+                <Edit
+                    employees={employees}
+                    selectedEmployee={selectedEmployee}
+                    setIsEditing={setIsEditing}
+                />
+            )}
+        </div>
+    )
+}
+
+export default Dashboard;
